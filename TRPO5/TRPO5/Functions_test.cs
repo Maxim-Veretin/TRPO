b1859550127281a762c0fafd051e0165dd04d5e8
Maxim-Veretin <maxim.veretin.2410@gmail.com>
2019-04-18 15:04:36 +0700

    complited

---
+++ TRPO5/TRPO5/Functions_test.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace TRPO5_test
+{
+    [TestFixture]
+    class Functions_test
+    {
+        [TestCase]
+        public void Convertik()
+        {
+            Functions f = new Functions();
+            Assert.AreEqual(7.62, f.Convertik(3));
+
+            var ex = Assert.Throws<Exception>(() => f.Convertik(-3));
+            Assert.That(ex.Message, Is.EqualTo("Входящее значение не может быть меньше, либо равным нулю."));
+        }
+
+        [TestCase]
+        public void Chetnost()
+        {
+            Functions f = new Functions();
+            Assert.AreEqual(true, f.Chetnost(4));
+            Assert.AreEqual(false, f.Chetnost(9));
+            Assert.AreEqual(true, f.Chetnost(-8));
+            Assert.AreEqual(false, f.Chetnost(-7));
+        }
+
+        [TestCase]
+        public void Ostatok()
+        {
+            Functions f = new Functions();
+            Assert.AreEqual(0, f.Ostatok(2,2));
+            Assert.AreEqual(1, f.Ostatok(5,2));
+            Assert.AreEqual(2, f.Ostatok(5, 3));
+            var ex = Assert.Throws<Exception>(() => f.Ostatok(2, 0));
+            Assert.That(ex.Message, Is.EqualTo("Делитель не должен быть равен нулю."));
+        }
+
+        [TestCase]
+        public void Max_Arr_El()
+        {
+            int[] arr = new int[] { };
+            int[] arr1 = new int[] { 1, 2, 3, 4, 5 };
+            int[] arr2 = new int[] { -1, -2, -3, -4, -5 };
+
+            Functions f = new Functions();
+            Assert.AreEqual(5, f.Max_Arr_El(arr1));
+            Assert.AreEqual(-1, f.Max_Arr_El(arr2));
+            var ex = Assert.Throws<Exception>(() => f.Max_Arr_El(arr));
+            Assert.That(ex.Message, Is.EqualTo("Пустой массив."));
+        }
+    }
+}
---
+++ TRPO5/TRPO5/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TRPO5_test
+{
+    class Functions
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        public double Convertik(double dm)
+        {
+            if (dm <= 0)
+            {
+                throw new Exception("Входящее значение не может быть меньше, либо равным нулю.");
+            }
+            return (dm * 2.54);
+        }
+
+        public bool Chetnost(int num1)
+        {
+            if (num1 % 2 == 0)
+                return true;
+            else
+                return false;
+        }
+
+        public int Max_Arr_El(int[] arr)
+        {
+            if (arr.Length == 0)
+                throw new Exception("Пустой массив.");
+
+            int max = arr[0];
+
+            for (int i = 0; i <= arr.Length - 1; i++)
+            {
+                if (max < arr[i])
+                    max = arr[i];
+                else
+                    continue;
+            }
+            return max;
+        }
+
+        public int Ostatok(int num1, int num2)
+        {
+            if(num2==0)
+                throw new Exception("Делитель не должен быть равен нулю.");
+            return num1 % num2;
+        }
+    }
+}
